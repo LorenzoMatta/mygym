5bfa04bb9977a25a236226a8920f1d2d1d0a06a3
Lorenzo <lorenzom04@gmail.com>
2023-03-02 11:15:26 +0100

    Class initialization and definition

---
+++ src/main/java/it/corso/mygym/model/Subscription.java
@@ -0,0 +1,45 @@
+package it.corso.mygym.model;
+
+
+import jakarta.validation.constraints.NotNull;
+import jdk.jfr.BooleanFlag;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.time.LocalDate;
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "subscription")
+public class Subscription implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String type;
+
+    private Double price;
+
+    @NotNull
+    private LocalDate startDate;
+
+    @NotNull
+    private LocalDate endDate;
+
+    @NotNull
+    @BooleanFlag
+    private boolean isPaid;
+
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "gym_id")
+    private Gym gym;
+}
